c12fd6bef927ed276bd9e18c07c8cda43fe7e06c
hai0901 <119472132+hai0901@users.noreply.github.com>
2023-11-29 06:35:49 +0700

    Header
    Logo, navigation bar, search bar, account button

---
+++ public/js/nav.js
@@ -0,0 +1,22 @@
+var dropdownVisible = false;
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  dropdownVisible = !dropdownVisible; // Toggle the dropdown state
+  dropdown.classList.toggle("show", dropdownVisible);
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.user')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        dropdownVisible = false; // Update the dropdown state when closed
+      }
+    }
+  }
+}
