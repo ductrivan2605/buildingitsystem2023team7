8f73a43138513de6babcde88be3284a5049083bb
hai0901 <119472132+hai0901@users.noreply.github.com>
2023-11-29 08:07:28 +0700

    wishlistform 1

--- public/js/nav.js
+++ public/js/nav.js
@@ -25,3 +25,36 @@ function toggleMenu() {
     var navList = document.getElementById("navList");
     navList.style.display = (navList.style.display === "none" || navList.style.display === "") ? "flex" : "none";
   }
+
+  function submitWishlist() {
+    const author = document.getElementById("author").value;
+    const title = document.getElementById("title").value;
+    const date = document.getElementById("date").value;
+    const image = document.getElementById("image").value;
+  
+    const wishlistData = {
+      author,
+      title,
+      date,
+      image,
+    };
+  
+    // Send wishlist data to the server (Node.js) using an AJAX request or fetch API
+    // Example using fetch:
+    fetch('/submit-wishlist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(wishlistData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle response from the server (e.g., show a success message)
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error:', error);
+    });
+  }
