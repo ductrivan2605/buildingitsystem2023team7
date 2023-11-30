25fce8e3efe4b4e2234f3c1e9651d8f6aa9b70af
ductrivan2605 <tri.van2605@gmail.com>
2023-11-30 16:58:25 +0700

    update authentication system

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const mongoose = require("mongoose");
 
-
-
 // Static Files
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,15 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mongoose
     // Books Database
-        mongoose.connect('mongodb+srv://thegalaxy1590:thegalaxy1590@librarybooks.pouktuc.mongodb.net/?retryWrites=true&w=majority')
+        mongoose.connect('mongodb+srv://thegalaxy1590:thegalaxy1590@librarybooks.pouktuc.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(() => console.log("Connected to MongoDB Atlas!"))
         .catch((error) => console.log(error.message));
 
 // Routes
 const CategoryRouter = require('./routes/admin/categoryRoute');
+const authRoutes = require('./routes/authRoutes');
 
 app.use("/category/", CategoryRouter);
-
+app.use("/auth", authRoutes)
 app.listen(3000, ()=>{
     console.log(`Server is running on port localhost:3000`);
 });
