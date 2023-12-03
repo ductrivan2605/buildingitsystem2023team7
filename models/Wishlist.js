4edea20626827a886b6fc690aa7d82ddee33265c
hai0901 <119472132+hai0901@users.noreply.github.com>
2023-12-03 17:37:32 +0700

    update wishlist models

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 const wishlistSchema = new mongoose.Schema({
-  authorWishlist: String,
-  titleWishlist: String,
-  dateWishlist: Date,
-  imageWishlist: String,
+  authorWishlist: { 
+    type: String
+  },
+  titleWishlist: {
+    type: String
+  },
+  dateWishlist: {
+    type: Date
+  },
+  imageWishlist: {
+    type: String
+  },
 });
 
 const WishlistItem = mongoose.model('WishlistItem', wishlistSchema);
