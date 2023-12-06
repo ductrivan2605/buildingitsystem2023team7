85e9d79b412acc237840666ecc7b24a60550b035
ductrivan2605 <tri.van2605@gmail.com>
2023-12-06 11:36:53 +0700

    changing some stuff related to project

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const mongoose = require("mongoose");
 
 
 // Static Files
-app.set("views", __dirname + "/views");
+// app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 
 // SetUp parse
@@ -17,14 +18,27 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mongoose
     // Books Database
-        mongoose.connect('mongodb+srv://thegalaxy1590:thegalaxy1590@librarybooks.pouktuc.mongodb.net/?retryWrites=true&w=majority')
+        mongoose.connect('mongodb+srv://thegalaxy1590:thegalaxy1590@librarybooks.pouktuc.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(() => console.log("Connected to MongoDB Atlas!"))
         .catch((error) => console.log(error.message));
 
 // Routes
 const CategoryRouter = require('./routes/admin/categoryRoute');
 
+<<<<<<< Updated upstream
 app.use("/category/", CategoryRouter);
+=======
+// Render
+app.get('/', (req,res) => {
+    res.render("signin")
+});
+app.get('/register', (req,res) => {
+    res.render("signup")
+});
+>>>>>>> Stashed changes
 
 app.listen(3000, ()=>{
     console.log(`Server is running on port localhost:3000`);
