77de7072bec4b1a014563e3fa0498edeea64a56b
hai0901 <119472132+hai0901@users.noreply.github.com>
2023-12-11 03:25:19 +0700

    fix the wishlist

--- routes/wishlistRouter.js
+++ routes/wishlistRouter.js
@@ -1,79 +1,74 @@
 // routes/wishlist.js
 const express = require('express');
+const router = express.Router();
 const WishlistItem = require('../models/Wishlist');
-const wishlistRouter = express.Router();
 
-// Route to handle form submission
-wishlistRouter.post('/submitWishlist', async (req, res) => {
-  try {
-    // Create a new WishlistItem instance with data from the form
-    const newWishlistItem = new WishlistItem({
-      authorWishlist: req.body.authorWishlist,
-      titleWishlist: req.body.titleWishlist,
-      dateWishlist: req.body.dateWishlist,
-      imageWishlist: req.body.imageWishlist,
-    });
 
-    // Save the new WishlistItem to MongoDB
-    await newWishlistItem.save();
-
-    console.log('New wishlist item submitted:', newWishlistItem);
-    res.redirect('/wishlistadmin.html'); 
-  } catch (error) {
-    console.error('Error submitting wishlist item:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Route to handle approving a wishlist item
-wishlistRouter.post('/approveWishlist/:id', async (req, res) => {
-  try {
-    const wishlistItemId = req.params.id;
-
-    // Find the wishlist item by ID in MongoDB
-    const wishlistItem = await WishlistItem.findById(wishlistItemId);
-
-    if (!wishlistItem) {
-      console.log('Wishlist item not found');
-      return res.status(404).send('Wishlist item not found');
+/* router.post('/', async (req, res) => {
+    try {
+      const { author, title, date, image } = req.body;
+      console.log('Received data:', { author, title, date, image });
+  
+      // Create a new wishlist item
+      const newWishlistItem = new WishlistItem({
+        author,
+        title,
+        date,
+        image,
+      });
+  
+      // Save the wishlist item to the database
+      await newWishlistItem.save();
+  
+      console.log('Wishlist item saved successfully');
+      res.status(201).json({ message: 'Wishlist item submitted successfully' });
+    } catch (error) {
+      console.error('Error submitting wishlist item:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  }); */
+  
+// Search wishlist item by ID
 
-    // Update the 'approved' field to true
-    wishlistItem.approved = true;
 
-    // Save the updated wishlist item
-    await wishlistItem.save();
+  router.post('/', async (req, res) => {
+    try {
+      const { authorWishlist, titleWishlist, dateWishlist, imageWishlist } = req.body;
+  
+      // Log received data
+      console.log('Received data:', { authorWishlist, titleWishlist, dateWishlist, imageWishlist });
 
-    console.log('Wishlist item approved:', wishlistItem);
-    res.redirect('/wishlistadmin.html'); // Redirect to admin dashboard after approval
-  } catch (error) {
-    console.error('Error approving wishlist item:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Route to handle deleting a wishlist item
-wishlistRouter.post('/deleteWishlist/:id', async (req, res) => {
-  try {
-    const wishlistItemId = req.params.id;
-
-    // Find the wishlist item by ID in MongoDB
-    const wishlistItem = await WishlistItem.findById(wishlistItemId);
-
-    if (!wishlistItem) {
-      console.log('Wishlist item not found');
-      return res.status(404).send('Wishlist item not found');
+      const newWishlistItem = new WishlistItem({
+        authorWishlist: authorWishlist,
+        titleWishlist: titleWishlist,
+        dateWishlist: dateWishlist,
+        imageWishlist: imageWishlist,
+      });
+  
+      // Save the wishlist item to the database
+      await newWishlistItem.save();
+  
+      // Respond immediately without interacting with the database
+      res.status(201).json({ message: 'Wishlist item submitted successfully' });
+    } catch (error) {
+      console.error('Error in route handling:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  });
 
-    // Remove the wishlist item from MongoDB
-    await wishlistItem.remove();
 
-    console.log('Wishlist item deleted:', wishlistItem);
-    res.redirect('/wishlistadmin.html'); // Redirect to admin dashboard after deletion
-  } catch (error) {
-    console.error('Error deleting wishlist item:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+  
+/* 
+router.post('/', async (req, res) => {
+    try {
+      console.log('Received data:', req.body);
+  
+      res.status(200).json({ message: 'Received data successfully' });
+    } catch (error) {
+      console.error('Error in route handling:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+   */
 
-module.exports = wishlistRouter;
+module.exports = router;
