85e530670f92c6ed01cd7e3c3c49c6c579dd2daa
Jimin76 <118871123+Jimin76@users.noreply.github.com>
2023-12-13 05:59:48 +0700

    add 404 page. modify register, login page
    I added 404page.
    For the login page, I have change the color theme and add Booktopia logo, and fixed error caused by combination with header.
    did same thing with the register page too

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,15 +1,43 @@
 showContent('general');
 
 function showContent(category) {
-    const contentElements = document.querySelectorAll('.content');
-  
-    contentElements.forEach((element) => {
-      element.style.display = 'none';
-    });
-  
-    const selectedContent = document.getElementById(category);
-    if (selectedContent) {
-      selectedContent.style.display = 'block';
-    }
+  const contentElements = document.querySelectorAll('.content');
+
+  contentElements.forEach((element) => {
+    element.style.display = 'none';
+  });
+
+  const selectedContent = document.getElementById(category);
+  if (selectedContent) {
+    selectedContent.style.display = 'block';
   }
-  
+}
+
+//image upload
+function handleImageUpload() {
+  const input = document.getElementById('uploadInput');
+  const image = document.getElementById('profileImage');
+
+
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+
+      image.src = e.target.result;
+    };
+
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+
+function resetImage() {
+  const image = document.getElementById('profileImage');
+
+  image.src = '/public/images/userDefault.jpg';
+}
+
+
+document.getElementById('uploadInput').addEventListener('change', handleImageUpload);
